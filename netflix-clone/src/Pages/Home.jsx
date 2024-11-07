031925217d6323cb2bdccaa15c4abdf5e9413e2d
EzraTechA <ezrawork12@gmail.com>
2024-11-07 18:05:13 +0300

    commit

---
+++ netflix-clone/src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../Components/Header/header'
+import Footer from '../Components/Header/Footer/Footer'
+function Home() {
+    return (
+        <>
+            <Header />
+            <Footer />
+        </>
+    )
+}
+
+export default Home
